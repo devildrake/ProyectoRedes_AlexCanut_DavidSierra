4073052fb1f928db4dc1c9ce986ccf6ef8988d46
Devildrake <davidsierragonzalez@enti.cat>
2017-01-14 17:05:09 +0100

    He cambiado de opinion con el como proceder.. ahora copiaré el codigo de clientBueni y ahi colocare el pacman

--- Cliente_Xarxes_DavidSierra_AlexCanut/Cliente_Xarxes_DavidSierra_AlexCanut/Pacman_AlexCanut_DavidSierra.cpp
+++ Cliente_Xarxes_DavidSierra_AlexCanut/Cliente_Xarxes_DavidSierra_AlexCanut/Pacman_AlexCanut_DavidSierra.cpp
@@ -9,25 +9,6 @@
 #include <stdlib.h>
 //#include <stdlib.h>
 
-//Librerias conexion
-#define WIN32_LEAN_AND_MEAN
-
-#include <windows.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-//#include <stdlib.h>
-//#include <stdio.h>
-
-
-// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
-#pragma comment (lib, "Ws2_32.lib")
-#pragma comment (lib, "Mswsock.lib")
-#pragma comment (lib, "AdvApi32.lib")
-
-
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "27015"
-//Cierre librerías conexion
 
 #define ARRIBA     72      // CONSTANTS AMB LES FLETXES DEL TECLAT
 #define IZQUIERDA  75
@@ -461,7 +442,6 @@ bool validate(string aName){
 
 void main() {
 	jugadorT top10[10];
-
 
 
 	for (int i = 0; i < 10; i++) {
